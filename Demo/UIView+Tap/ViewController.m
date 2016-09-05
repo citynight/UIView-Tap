53d74a5cfc94e24f168ebb35bee79e63e11a7790
LiXiaozheng <mekor@live.cn>
2016-09-05 09:48:13 +0800

    添加说明

--- Demo/UIView+Tap/ViewController.m
+++ Demo/UIView+Tap/ViewController.m
@@ -19,26 +19,26 @@
     [super viewDidLoad];
     self.title = @"视图点击";
     
-UIView *redView = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
-redView.backgroundColor = [UIColor redColor];
-[redView addTapBlock:^(UIView* obj) {
-    NSLog(@"redView%@",obj.backgroundColor);
-}];
-[self.view addSubview:redView];
-
-UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(50, 250, 100, 100)];
-imageView.image = [UIImage imageNamed:@"icon"];
-[imageView addTapBlock:^(UIImageView* obj) {
-    NSLog(@"imageView:\n%@",obj.image);
-}];
-[self.view addSubview:imageView];
-
-UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(150, 400, 100, 100)];
-label.text = @"这是label,点击这里...";
-[label addTapBlock:^(UILabel* obj) {
-    NSLog(@"label:\n%@",obj.text);
-}];
-[self.view addSubview:label];
+    UIView *redView = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    redView.backgroundColor = [UIColor redColor];
+    [redView addTapBlock:^(UIView* obj) {
+        NSLog(@"redView%@",obj.backgroundColor);
+    }];
+    [self.view addSubview:redView];
+    
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(50, 250, 100, 100)];
+    imageView.image = [UIImage imageNamed:@"icon"];
+    [imageView addTapBlock:^(UIImageView* obj) {
+        NSLog(@"imageView:\n%@",obj.image);
+    }];
+    [self.view addSubview:imageView];
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(150, 400, 100, 100)];
+    label.text = @"这是label,点击这里...";
+    [label addTapBlock:^(UILabel* obj) {
+        NSLog(@"label:\n%@",obj.text);
+    }];
+    [self.view addSubview:label];
 
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # UIView-Tap
 给UIView添加点击事件
+
+给UIView添加一个点击事件的扩展,说明一点在这个分类中如果想添加点击事件分类里面强制`userInteractionEnabled = YES`
+使用到的技术,动态添加属性.get`objc_getAssociatedObject`,set`objc_setAssociatedObject`方法.
+使用方法:
+
+```
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(150, 400, 100, 100)];
+    label.text = @"这是label,点击这里...";
+    [label addTapBlock:^(UILabel* obj) {
+        NSLog(@"label:\n%@",obj.text);
+    }];
+    [self.view addSubview:label];
+```
+
+
